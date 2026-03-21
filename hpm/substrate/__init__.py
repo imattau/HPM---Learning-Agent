5d18ab2f4110a9d500fb17023623882a0f7522fd
imattau <imatt.au@protonmail.com>
2026-03-21 18:29:50 +1100

    feat: export LinguisticSubstrate and MathSubstrate; add dependencies to requirements.txt

---
+++ hpm/substrate/__init__.py
@@ -0,0 +1,13 @@
+from .wikipedia import WikipediaSubstrate
+from .pypi import PyPISubstrate
+from .local_file import LocalFileSubstrate
+from .linguistic import LinguisticSubstrate
+from .math import MathSubstrate
+
+__all__ = [
+    'WikipediaSubstrate',
+    'PyPISubstrate',
+    'LocalFileSubstrate',
+    'LinguisticSubstrate',
+    'MathSubstrate',
+]
