30485dbf32dc74a74b1811e912ded7e6f1de97cf
imattau <imatt.au@protonmail.com>
2026-03-23 07:17:35 +1100

    feat: re-export hierarchical symbols from hpm.agents

--- hpm/agents/__init__.py
+++ hpm/agents/__init__.py
@@ -1,3 +1,17 @@
 from .actor import DecisionalActor
+from .hierarchical import (
+    LevelBundle,
+    encode_bundle,
+    extract_bundle,
+    HierarchicalOrchestrator,
+    make_hierarchical_orchestrator,
+)
 
-__all__ = ["DecisionalActor"]
+__all__ = [
+    "DecisionalActor",
+    "LevelBundle",
+    "encode_bundle",
+    "extract_bundle",
+    "HierarchicalOrchestrator",
+    "make_hierarchical_orchestrator",
+]
